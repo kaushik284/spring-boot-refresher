21f35ea2589a29671048e022b28a92959962fc5c
kaushik <kaushik.mitra28@gmail.com>
2025-07-23 22:33:45 +0530

    changing map to Map<Games, List<GameGenres>>

--- state-machine-demo/src/main/java/learning/kaushik/state_machine_demo/configs/GameGenreProperties.java
+++ state-machine-demo/src/main/java/learning/kaushik/state_machine_demo/configs/GameGenreProperties.java
@@ -1,6 +1,7 @@
 package learning.kaushik.state_machine_demo.configs;
 
 import learning.kaushik.state_machine_demo.enums.GameGenre;
+import learning.kaushik.state_machine_demo.enums.Games;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -12,12 +13,12 @@ import java.util.Map;
 /**
  * Load property game.names into names Map.<br/>
  * Example game.names.broken_arrow=strategy,real_time_tactics<br/> will be
- * loaded into names Map as {"broken_arrow": [GameGenre.STRATEGY, GameGenre.REAL_TIME_TACTICS]}
+ * loaded into names Map as {Games.broken_arrow: [GameGenre.STRATEGY, GameGenre.REAL_TIME_TACTICS]}
  */
 @Configuration
 @ConfigurationProperties(prefix = "game")
 @Getter
 @Setter
 public class GameGenreProperties {
-    private Map<String, List<GameGenre>> names;
+    private Map<Games, List<GameGenre>> names;
 }
---
+++ state-machine-demo/src/main/java/learning/kaushik/state_machine_demo/enums/Games.java
@@ -0,0 +1,11 @@
+package learning.kaushik.state_machine_demo.enums;
+
+public enum Games {
+    COH,
+    BROKEN_ARROW,
+    counter_strike,
+    yakuza0,
+    torchlight,
+    sudden_strike_2,
+    river_city_ransom;
+}
